2f662b57e93f1b891a8b79b0f0be9a64e43993e9
Marc Linster <marc@marclinster.com>
2025-08-07 12:19:00 +0200

    Cleaned up data set definitions. Removed obsolete product_inventory.sql file

--- psql_scripts/data_sets/west_ecommerce_data/west_sales/sales_transaction_line.sql
+++ psql_scripts/data_sets/west_ecommerce_data/west_sales/sales_transaction_line.sql
@@ -1,25 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 18beta1 (Debian 18~beta1-1.pgdg120+1)
--- Dumped by pg_dump version 18beta1 (Debian 18~beta1-1.pgdg120+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Data for Name: sales_transaction_line; Type: TABLE DATA; Schema: west_sales; Owner: postgres
---
 
 INSERT INTO west_sales.sales_transaction_line VALUES
 	('west_1', 'west_1', 52, 1, 5.65),
