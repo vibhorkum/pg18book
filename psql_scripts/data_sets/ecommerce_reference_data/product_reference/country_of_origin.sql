8da831991f965d5e11523638bec02801e121aed9
Marc Linster <marc@marclinster.com>
2025-08-17 20:07:19 +0200

    Added country of origin table in the ecommerce reference model (not replicated yet)

---
+++ psql_scripts/data_sets/ecommerce_reference_data/product_reference/country_of_origin.sql
@@ -0,0 +1,21 @@
+INSERT INTO product_reference.country_of_origin (product_brand_id, alpha3_code, name)
+    VALUES
+        (1, 'USA', 'United States of America'), -- Gap
+        (2, 'DEU', 'Germany'),                  -- Boss
+        (3, 'USA', 'United States of America'), --'Diesel
+        (4, 'FRA', 'FRANCE'),                   --'Aéropostale
+        (5, 'USA', 'United States of America'),      -- Levis
+        (6, 'USA', 'United States of America'),      -- Nike
+        (7, 'DEU', 'Germany'), -- Adidas
+        (8, 'ESP', 'Spain'), -- Zara
+        (9, 'SWE', 'Sweden'), -- H&M
+        (10, 'JPN', 'Japan'), -- Uniqlo
+        (11, 'USA', 'United States of America'), -- Calvin Klein
+        (12, 'USA', 'United States of America'), -- Tommy Hilfiger
+        (13, 'USA', 'United States of America'), -- Polo Ralph Lauren
+        (14, 'FRA', 'FRANCE'), -- Lacoste
+        (15, 'USA', 'United States of America'), -- Under Armour
+        (16, 'GBR', 'United Kingdom'), -- Charles Tyrwhitt
+        (17, 'SWE', 'Sweden'), -- Eton
+        (18, 'ITA', 'Italy'); -- Brioni
+
