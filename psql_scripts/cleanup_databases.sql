27c2d34952c0207faeb3e8358e4814abcd427746
Marc Linster <marc@marclinster.com>
2025-11-23 20:01:44 +0100

    Clean up
    Added central_analytics_bcp (which results from running the exercises in Chapter 12) to the list

--- psql_scripts/cleanup_databases.sql
+++ psql_scripts/cleanup_databases.sql
@@ -12,17 +12,11 @@
 \set db_to_drop_3 'east_ecommerce_data'
 \set db_to_drop_4 'central_analytics'
 \set db_to_drop_5 'aidb'
+\set db_to_drop_6 'central_analytics_bcp'
 
 \echo '*** Database Cleanup Script Started ***'
-\echo 'WARNING: This script will permanently delete databases:' :'db_to_drop_1'', ':'db_to_drop_2', ', ':'db_to_drop_3' ', ':'db_to_drop_4'
+\echo 'WARNING: This script will permanently delete databases:' :'db_to_drop_1'', ':'db_to_drop_2', ', ':'db_to_drop_3' ', ':'db_to_drop_4', ', ':'db_to_drop_5', ', ':'db_to_drop_6'
 
-\c postgres
-
-\set db_to_drop_1 'ecommerce_reference_data'
-\set db_to_drop_2 'west_ecommerce_data'
-\set db_to_drop_3 'east_ecommerce_data'
-\set db_to_drop_4 'central_analytics'
-\set db_to_drop_5 'aidb'
 
 -- =================================================================
 --  Step 1: Terminate connections and drop the first database
@@ -33,7 +27,8 @@ DROP DATABASE IF EXISTS :db_to_drop_1 WITH (FORCE);
 DROP DATABASE IF EXISTS :db_to_drop_2 WITH (FORCE);
 DROP DATABASE IF EXISTS :db_to_drop_3 WITH (FORCE);
 DROP DATABASE IF EXISTS :db_to_drop_4 WITH (FORCE);
-DROP DATABASE IF EXISTS :db_to_drop_5 WITH (FORCE);
+DROP DATABASE IF EXISTS :db_to_drop_5 WITH (FORCE); 
+DROP DATABASE IF EXISTS :db_to_drop_6 WITH (FORCE);
 
 
 \echo '--> Verifying that databases are gone. The following list should not contain the dropped databases:'
